80e920693df799281edc74a90b1f316211dc0cc9
thomas_jakemeyn <thomas.jakemeyn@gmail.com>
2016-04-11 21:23:57 +0200

    Enriched README

--- README.md
+++ README.md
@@ -27,9 +27,9 @@ The NightShift core object provides some utilities.
 
 ### functions
 #### factoryOf(ConstructorFn)
-The usage of the `new` keyword prevents a module from being testable in isolation. A solution is to delegate the instantiation to a factory which can then be injected in the module and mocked for the testing.
+The usage of the `new` keyword prevents a module from being testable in isolation. Using `new` makes it impossible to test a module without (re-)testing the delegate it instantiates. A solution is to encapsulate the instantiation in a factory which can then be injected in the module and mocked for the testing.
 
-The method `nightShift.functions.factoryOf(ConstructorFn)` creates a factory that can instantiate objects by invoking the given constructor function with the arguments it receives.
+The method `nightShift.functions.factoryOf(ConstructorFn)` creates a factory that can instantiate objects by invoking the given constructor function with the parameters it receives.
 
 ```javascript
 it('should return a factory for the specified constructor function', function () {
@@ -39,5 +39,27 @@ it('should return a factory for the specified constructor function', function ()
     };
     var factory = nightShift.functions.factoryOf(Point);
     expect(factory(10, 20)).toEqual(new Point(10, 20));
+});
+```
+
+### getParamNames(fn)
+The method `nightShift.functions.getParamNames(fn)` returns an array listing the names of the parameters of the given function. The array is empty if the function does not expect any parameter.
+
+```javascript
+it('should return the names of the parameters of the specified function', function () {
+    expect(nightShift.functions.getParamNames(function () {})).toEqual([]);
+    expect(nightShift.functions.getParamNames(function (one) {})).toEqual(['one']);
+    expect(nightShift.functions.getParamNames(function (one, two) {})).toEqual(['one', 'two']);
+});
+```
+
+### getNbOfParams(fn)
+The method `nightShift.functions.getNbOfParams(fn)` returns the number of parameters which are expected by the given function.
+
+```javascript
+it('should return the number of parameters of the specified function', function () {
+    expect(nightShift.functions.getNbOfParams(function () {})).toEqual(0);
+    expect(nightShift.functions.getNbOfParams(function (one) {})).toEqual(1);
+    expect(nightShift.functions.getNbOfParams(function (one, two) {})).toEqual(2);
 });
 ```
--- src/utils/functions.js
+++ src/utils/functions.js
@@ -3,14 +3,15 @@
 module.exports = function createFunctionUtils() {
 
     /**
-     * The usage of the 'new' keyword prevents a module from being testable in isolation. A solution is to delegate the
-     * instantiation to a factory which can then be injected in the module and mocked for the testing.
+     * The usage of the 'new' keyword prevents a module from being testable in isolation. Using 'new' makes it
+     * impossible to test a module without (re-)testing the delegate it instantiates. A solution is to encapsulate the
+     * instantiation in a factory which can then be injected in the module and mocked for the testing.
      *
      * This method creates a factory that can instantiate objects by invoking the given constructor function with the
-     * arguments it receives.
+     * parameters it receives.
      *
-     * @param Constructor the constructor function to use for instantiating objects
-     * @returns {Function} a factory function which takes the same arguments as the constructor function
+     * @param {function} Constructor the constructor function to use for instantiating objects
+     * @returns {function} a factory function that expects the same parameters as the given constructor function
      */
     function factoryOf(Constructor) {
         return function () {
@@ -21,6 +22,14 @@ module.exports = function createFunctionUtils() {
         };
     }
 
+    /**
+     * This method returns an array listing the names of the parameters of the given function. The array is empty if the
+     * function does not expect any parameter.
+     *
+     * @param {function} fn the function to retrieve the parameters names for
+     * @returns {Array|{index: number, input: string}} an array listing the names of the parameters of the given
+     * function, or an empty array if the function does not expect any parameter
+     */
     function getParamNames(fn) {
         var STRIP_COMMENTS = /((\/\/.*$)|(\/\*[\s\S]*?\*\/))/mg;
         var fnString = fn.toString().replace(STRIP_COMMENTS, '');
@@ -28,6 +37,12 @@ module.exports = function createFunctionUtils() {
         return result || [];
     }
 
+    /**
+     * This method returns the number of parameters which are expected by the given function.
+     *
+     * @param {function} fn the function to retrieve the number of parameters for
+     * @returns {Number} the number of parameters which are expected by the given function
+     */
     function getNbOfParams(fn) {
         return getParamNames(fn).length;
     }
