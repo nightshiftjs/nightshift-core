9f7f982cf3652a4afe9c647808ea460b074c8852
thomas_jakemeyn <thomas.jakemeyn@gmail.com>
2016-04-14 20:49:02 +0200

    Modified description in specification

--- tests/utils/functions.spec.js
+++ tests/utils/functions.spec.js
@@ -4,7 +4,7 @@ describe('The function utilities', function () {
 
     var functions = require('../../src/utils/functions')();
 
-    it('should return a factory for the specified constructor function', function () {
+    it('should return a factory for the given constructor function', function () {
         var Point  = function (x, y) {
             this.x = x;
             this.y = y;
@@ -13,13 +13,13 @@ describe('The function utilities', function () {
         expect(factory(10, 20)).toEqual(new Point(10, 20));
     });
 
-    it('should return the names of the parameters of the specified function', /* jshint unused:false */ function () {
+    it('should return the names of the parameters of the given function', /* jshint unused:false */ function () {
         expect(functions.getParamNames(function () {})).toEqual([]);
         expect(functions.getParamNames(function (one) {})).toEqual(['one']);
         expect(functions.getParamNames(function (one, two) {})).toEqual(['one', 'two']);
     });
 
-    it('should return the number of parameters of the specified function', /* jshint unused:false */ function () {
+    it('should return the number of parameters of the given function', /* jshint unused:false */ function () {
         expect(functions.getNbOfParams(function () {})).toEqual(0);
         expect(functions.getNbOfParams(function (one) {})).toEqual(1);
         expect(functions.getNbOfParams(function (one, two) {})).toEqual(2);
